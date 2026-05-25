e6ffd24d385b41c5f020eef2aea293bd05dd2b0c
Alan Carey <alancarey@pm.me>
2026-05-25 22:24:14 +0100

    Add third guardian code

--- 8. Lists/ex_08_02.py
+++ 8. Lists/ex_08_02.py
@@ -7,5 +7,8 @@ except:
 
 for line in fhand:
     words = line.split()
-    if len(words) < 3 or words[0] != "From" : continue 
+    # print('Debug:', words)
+    if len(words) == 0 : continue
+    if words[0] != "From" : continue
+    if len(words) < 3 : continue    
     print(words[2])
