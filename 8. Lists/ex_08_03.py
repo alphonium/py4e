cc4a26af43f7a10fd539d1b6c6bfd3564b06dd14
Alan Carey <alancarey@pm.me>
2026-05-25 22:25:52 +0100

    Clean up guardian code to one statement

---
+++ 8. Lists/ex_08_03.py
@@ -0,0 +1,11 @@
+fname = input("Enter the file name: ")
+try:
+    fhand = open(fname)
+except:
+    print("File cannot be opened:" , fname)
+    exit()
+
+for line in fhand:
+    words = line.split()
+    if len(words) < 3 or words[0] != "From" : continue 
+    print(words[2])
