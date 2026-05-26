8b86f842a7b01bfc438ed6c4031fccacfca03872
Alan Carey <alancarey@pm.me>
2026-05-26 20:31:59 +0100

    Streamlined code even more

--- 9. Dictionaries/ex_09_05.py
+++ 9. Dictionaries/ex_09_05.py
@@ -12,8 +12,11 @@ for line in fhand:
     words = line.split()
     if len(words) < 2 or words[0] != "From" : continue
     email = words[1]
+    """ 
     for i in range(len(email)):
         if email[i] == "@":
-            domain = email[i+1:]
+            domain = email[i+1:] 
+    """
+    domain = email.split('@')[1] # More efficient way than the for loop with range        
     domains[domain] = domains.get(domain, 0) + 1
 print(domains)
