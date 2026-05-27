72c43059e6511da6e72628d3f0439c511c005472
Alan Carey <alancarey@pm.me>
2026-05-27 18:18:06 +0100

    Use tuples to sort dictionary

--- 10. Tuples/ex_10_02.py
+++ 10. Tuples/ex_10_02.py
@@ -7,9 +7,12 @@ except:
     print("File cannot be opened:", fname)
     exit()
 
-senders = dict()
+hours = dict()
 for line in fhand:
     words = line.split()
-    if len(words) < 2 or words[0] != "From" : continue
-    senders[words[1]] = senders.get(words[1], 0) + 1
-print(senders)
+    if len(words) < 6 or words[0] != "From" : continue
+    hour = words[5].split(':')[0]
+    hours[hour] = hours.get(hour, 0) + 1
+
+for hour, count in sorted(list(hours.items())):
+    print(hour, count)
