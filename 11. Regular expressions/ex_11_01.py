7e813a6bafe389c8b8564f75409d04cc69417570
Alan Carey <32581501+alphonium@users.noreply.github.com>
2026-05-29 10:49:26 +0000

    Add user input for query

--- 11. Regular expressions/ex_11_01.py
+++ 11. Regular expressions/ex_11_01.py
@@ -1,11 +1,11 @@
 import re
 
 fhand = open('../data/mbox.txt')
-
+query = input("Enter query: ")
 count = 0
 for line in fhand:
     line = line.strip()
-    x = re.findall('^Author', line)
+    x = re.findall(query, line)
     if len(x) > 0:
         count += 1
     
