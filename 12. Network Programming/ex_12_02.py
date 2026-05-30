08695773a8d0b2b0957393519bbc3f1fed15d8b5
Alan Carey <alancarey@pm.me>
2026-05-30 19:07:06 +0100

    Limit to 3000 character displayed

--- 12. Network Programming/ex_12_02.py
+++ 12. Network Programming/ex_12_02.py
@@ -1,6 +1,8 @@
 import socket
 
 url = input("Enter the URL: ")
+if len(url) < 1:
+    url = 'http://data.pr4e.org/romeo.txt'
 try:
     host = url.split('/')[2]
 except:
@@ -11,14 +13,25 @@ print()
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect((host, 80))
-cmd = 'GET ' + url + ' HTTP/1.0\r\n\r\n'
-print(cmd)
-mysock.send(cmd.encode())
+mysock.send(('GET '+url+' HTTP/1.0\r\n\r\n').encode())
+
+count = 0
+limit = 3000
+displayed = 0
 
 while True:
     data = mysock.recv(512)
     if len(data) < 1:
         break
-    print(data.decode(),end='')
+    text = data.decode()
+    for ch in text:
+        count += 1
+        if displayed < limit:
+            print(ch, end='')
+            displayed += 1 
+    
+print('\n')
+print("Total characters received:", count)
+print("Total characters displayed:", count if count <= limit else limit)
 
 mysock.close()
